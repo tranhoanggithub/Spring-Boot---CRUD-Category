e52eeea3a77d26bf218ddf7aae578d8f28186932
tranhoanggithub <thoang.19i@cit.udn.vn>
2023-03-21 17:25:03 +0700

    first commit

---
+++ SpringBoot-Video/src/main/java/vn/iotstart/SpringBootVideo/SpringBootVideoApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstart.SpringBootVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVideoApplication.class, args);
+	}
+
+}
